1ed6f66f31447da668fadecbccfe60db6efe701b
Seng Kuang Yap <syap0014@student.monash.edu>
2023-05-10 13:38:59 +1000

    implemented databaseProtocol

--- BarugahDrip2.0/Database/DatabaseProtocol.swift
+++ BarugahDrip2.0/Database/DatabaseProtocol.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import UIKit
+
+enum DatabaseChange{
+    /*
+     Used to define what change has been done to the database
+     */
+    case add
+    case remove
+    case update
+}
+
+enum ListenerType{
+    /*
+     Used to specify type of data our listener will be dealing with
+     */
+    case all
+    case outfits
+    case garment
+    case outfit
+}
+
+protocol DatabaseListener: AnyObject {
+    var listenerType: ListenerType {get set}
+    
+    ///When there is a change in the wardrobe
+    func onGarmentChange(change: DatabaseChange, garments: [Garment])
+    
+    ///When there is a change in the outfits list
+    func onOutfitsChange(change: DatabaseChange, outfits: [Outfit])
+    
+    ///When there is a change in the garments that make up the outfit
+    func onOutfitGarmentsChange(change: DatabaseChange, garments: [Garment])
+
+}
+
+protocol DatabaseProtocol: AnyObject {
+    
+    var currentOutfit: Outfit? {get set}
+    
+    func addGarment(name: String, price: Double, brand: String, size: String, numberOfWears: Int, datePurchased: String) -> Garment
+    func deleteGarment(garment: Garment)
+    
+    func addOutfit(price: Double, wears: Int, outfitName: String)
+    func deleteOutfit(outfit: Outfit)
+    
+    func addGarmentToOutfit(garment: Garment, outfit: Outfit) -> Bool
+    func deleteGarmentFromOutfit(garment: Garment, outfit: Outfit)
+    
+    func cleanup()
+    
+    func addListener(Listener: DatabaseListener)
+    func removeListener(Listener: DatabaseListener)
+    
+    func addUser(name: String)
+}
