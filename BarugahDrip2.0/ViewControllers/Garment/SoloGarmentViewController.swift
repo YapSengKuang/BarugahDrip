b19873c603d499036aba49cd2a16cb1ff8f47d2a
Seng Kuang Yap <syap0014@student.monash.edu>
2023-05-17 14:58:31 +1000

    added SOLOGarmentVC

---
+++ BarugahDrip2.0/ViewControllers/Garment/SoloGarmentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SoloGarmentViewController.swift
+//  BarugahDrip2.0
+//
+//  Created by Eskay Yap on 17/5/2023.
+//
+
+import UIKit
+
+class SoloGarmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
