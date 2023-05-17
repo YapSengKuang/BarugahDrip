d02c265e3dc8992cb061656e654cde2ab40efa58
Seng Kuang Yap <syap0014@student.monash.edu>
2023-05-17 13:13:10 +1000

    work

---
+++ BarugahDrip2.0/ViewControllers/Garment/GarmentCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GarmentCollectionViewCell.swift
+//  BarugahDrip2.0
+//
+//  Created by Eskay Yap on 17/5/2023.
+//
+
+import UIKit
+
+class GarmentCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
