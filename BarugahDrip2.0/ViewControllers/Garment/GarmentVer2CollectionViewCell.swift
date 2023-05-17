199da6cc7645076f8cd340d7ed289c7b433a9e09
Seng Kuang Yap <syap0014@student.monash.edu>
2023-05-17 13:48:24 +1000

    added garment swift files

---
+++ BarugahDrip2.0/ViewControllers/Garment/GarmentVer2CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GarmentVer2CollectionViewCell.swift
+//  BarugahDrip2.0
+//
+//  Created by Eskay Yap on 17/5/2023.
+//
+
+import UIKit
+
+class GarmentVer2CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ BarugahDrip2.0/ViewControllers/Garment/GarmentsVer2CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GarmentsVer2CollectionViewCell.swift
+//  BarugahDrip2.0
+//
+//  Created by Eskay Yap on 17/5/2023.
+//
+
+import UIKit
+
+class GarmentsVer2CollectionViewCell: UICollectionViewCell {
+    
+}
